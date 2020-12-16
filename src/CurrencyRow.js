adcc6f93b438df6a3a45568740645ee9e4862944
Your Name <you@example.com>
2020-12-16 20:10:26 +0530

    Currency Converter

---
+++ src/CurrencyRow.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const CurrencyRow=({currencyOptions,selectedCurrency,onChangeCurrency,amount,onChangeAmount})=> {
+    return (
+        <div>
+            <input type="number" className="input" value={amount} onChange={onChangeAmount}/>
+            <select value={selectedCurrency} onChange={onChangeCurrency}>
+            {currencyOptions.map(option=>(
+                <option key={option} value={option}>{option}</option>
+            ))}
+            
+            </select>
+        </div>
+    )
+}
+
+export default CurrencyRow
